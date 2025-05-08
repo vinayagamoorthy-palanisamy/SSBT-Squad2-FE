67d71f3f23938f898281e783ffbfa570402929de
vinayagamoorthy-palanisamy <vinayagamoorthy.palanisamy@photon.com>
2025-05-08 14:02:43 +0530

    define extract and version history page development

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,19 +4,22 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import SubmitCoreExtract from '../components/SubmitCoreExtract';
 import SubmitCustomExtract from '../components/SubmitCustomExtract';
 import ExtractCenterTable from '../components/ExtractCenterTable';
-import ViewEditExtractPage from '../components/ViewEditExtractPage';
-import MyModal from '../components/CustomModal';
+import CoreExtractStepper from '../components/CoreExtractStepper';
+import EditableTransferList from '../components/EditableTransferList';
+
+// import ViewExtract from '../components/ViewExtract';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/extract-center" replace />} />
       <Route path="/extract-center" element={<ExtractCenterTable />} />
+      <Route path="/create-extract" element={<CoreExtractStepper />} />
+      <Route path="/transfer-list" element={<EditableTransferList />} />
       <Route path="/submit-core-extract" element={<SubmitCoreExtract />} />
       <Route path="/submit-custom-extract" element={<SubmitCustomExtract />} />
-      <Route path="/view-extract" element={<ViewEditExtractPage />} />
-      <Route path="/edit-extract" element={<ViewEditExtractPage isEditable/>} />
-      <Route path="/modal" element={<MyModal/>} />
+      {/* <Route path="/view-extract" element={<ViewExtract />} /> */}
+      {/* <Route path="/modal" element={<MyModal/>} /> */}
     </Routes>
   );
 };
