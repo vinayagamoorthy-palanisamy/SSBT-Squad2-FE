fbdb320bba6b00b75e9554eb3da75dc39865be25
Bibhuti-Photon <bibhuti.sunari@photon.com>
2025-05-20 11:51:02 +0530

    added selection dropdown

---
+++ src/components/CreateExtract.jsx
@@ -0,0 +1,122 @@
+import { useMemo, useState } from "react";
+import { Box, Grid } from "@mui/material";
+import DropDown from "./DropDown";
+import { useNavigate } from "react-router-dom";
+import CoreExtractStepper from "./CoreExtractStepper";
+
+const CreateExtract = () => {
+  const navigate = useNavigate();
+  const [coreExtract, setCoreExtract] = useState({
+    coreExtract: "",
+    reuseExtract: "",
+    selectExtract: "",
+  });
+  const [showDefineExtract, setShowDefineExtract] = useState(false);
+
+  const coreExtractOptions = [
+    {
+      label: "Yes",
+      value: "yes",
+    },
+    {
+      label: "No",
+      value: "no",
+    },
+  ];
+
+  const extractList = [
+    {
+      label: "Extract1",
+      value: "extract1",
+    },
+    {
+      label: "Extract2",
+      value: "extract2",
+    },
+    {
+      label: "Extract3",
+      value: "extract3",
+    },
+    {
+      label: "Extract4",
+      value: "extract4",
+    },
+  ];
+
+  const handleCoreExtract = (event, type) => {
+    const { value } = event.target;
+    setCoreExtract((prevExtract) => ({
+      ...prevExtract,
+      [type]: value,
+    }));
+
+    if (
+      (type === "coreExtract" && value === "yes") ||
+      (type === "reuseExtract" && value === "no")
+    ) {
+      setShowDefineExtract(true);
+    } else {
+      setShowDefineExtract(false);
+    }
+  };
+
+  const dropDowns = useMemo(
+    () => [
+      {
+        label: "Core Extract",
+        options: coreExtractOptions,
+        value: coreExtract.coreExtract,
+        onChange: (e) => handleCoreExtract(e, "coreExtract"),
+        show: true,
+      },
+      {
+        label: "Reuse Core Extract",
+        options: coreExtractOptions,
+        value: coreExtract.reuseExtract,
+        onChange: (e) => handleCoreExtract(e, "reuseExtract"),
+        show: coreExtract.coreExtract === "no",
+      },
+      {
+        label: "Select Core Extract",
+        options: extractList,
+        value: coreExtract.selectExtract,
+        onChange: (e) => handleCoreExtract(e, "selectExtract"),
+        show: coreExtract.reuseExtract === "yes",
+        selectStyle: { width: "180px" },
+      },
+    ],
+    [
+      coreExtract.coreExtract,
+      coreExtract.reuseExtract,
+      coreExtract.selectExtract,
+    ]
+  );
+
+  return (
+    <>
+      <Box sx={{ background: "white", padding: "3px 6px", color: "#101114" }}>
+        <p>State Street / Extract Center / Create Extract</p>
+      </Box>
+      <Box sx={{ p: "3rem" }}>
+        <Grid container spacing={2}>
+          {dropDowns.map(
+            (dropdown, index) =>
+              dropdown.show && (
+                <Grid item key={index}>
+                  <DropDown
+                    label={dropdown.label}
+                    options={dropdown.options}
+                    value={dropdown.value}
+                    handleChange={dropdown.onChange}
+                  />
+                </Grid>
+              )
+          )}
+        </Grid>
+        {showDefineExtract && <CoreExtractStepper />}
+      </Box>
+    </>
+  );
+};
+
+export default CreateExtract;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,17 +4,18 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import SubmitCoreExtract from '../components/SubmitCoreExtract';
 import SubmitCustomExtract from '../components/SubmitCustomExtract';
 import ExtractCenterTable from '../components/ExtractCenterTable';
-import CoreExtractStepper from '../components/CoreExtractStepper';
+//import CoreExtractStepper from '../components/CoreExtractStepper';
 import EditableTransferList from '../components/EditableTransferList';
 
 import CreateWorkflow from '../components/CreateWorkflow';
+import CreateExtract from '../components/CreateExtract';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/extract-center" replace />} />
       <Route path="/extract-center" element={<ExtractCenterTable />} />
-      <Route path="/create-extract" element={<CoreExtractStepper />} />
+      <Route path="/create-extract" element={<CreateExtract />} />
       <Route path="/transfer-list" element={<EditableTransferList />} />
       <Route path="/submit-core-extract" element={<SubmitCoreExtract />} />
       <Route path="/submit-custom-extract" element={<SubmitCustomExtract />} />
